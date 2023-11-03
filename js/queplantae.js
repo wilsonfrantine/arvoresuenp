e662ca1765c0f56e6a676c4191bd1c958fcf457c
Wilson Frantine-Silva <65643259+wilsonfrantine@users.noreply.github.com>
2023-11-03 19:03:48 -0300

    Update queplantae.js

--- js/queplantae.js
+++ js/queplantae.js
@@ -61,7 +61,7 @@ document.addEventListener('DOMContentLoaded', function() {
                     // resultContainer.textContent = row.join(', '); // Exemplo de exibição
                     specieDiv.innerHTML = '<span font-size=2vw>' + "Espécie: "+'</span>' + '<i>'+row[0]+'</i>';
                     commonNameDiv.innerHTML = '<span font-size=2vw>' + "Nome Comum: " + '</span>' + row[2];
-                    familyDiv.textContent = '<span font-size=2vw>'+ "Família: "+ '</span>' + row[3];
+                    familyDiv.textContent = '<span font-size=2vw>' + "Família: " + '</span>' + row[3];
                     // typeDiv.textContent = "<span font-size=2vw>Aplicação: </span>"+ row[4];
                     descricaoDiv.textContent = '<span font-size=2vw>' + "Descrição: " + '</span> <p></p>' + row[6];
                     
